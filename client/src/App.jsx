ab870a0367dd2ce6de633b6c941717d4447619d7
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-10-21 09:23:17 +0200

    completed trhe Navbar ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,22 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
+import Header from './components/Header'
+import Interview from './pages/Interview'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/interview" element={<Interview />}></Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
+import logo from '../assets/futurepath-logo.png'
+import { BsSearch } from 'react-icons/bs'
+import { FaMoon } from 'react-icons/fa'
+
+function Header() {
+  const path = useLocation().pathname
+  return (
+    <Navbar className="border-b-2 pb-2 pt-2">
+      <Link to="/" className="self-center w-[180px] sm:w-[250px]">
+        <img src={logo} alt="futurepath-logo" />
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={BsSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button color="gray" className="w-12 h-10 lg:hidden rounded-full">
+        <BsSearch />
+      </Button>
+
+      <div className="flex gap-2 md:order-2">
+        <Button
+          //gradientMonochrome="purple"
+          className="w-12 h-10 hidden sm:inline rounded-full"
+          color="gray"
+        >
+          <FaMoon />
+        </Button>
+
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue" className="">
+            Sign In
+          </Button>
+        </Link>
+
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/interview'} as={'div'}>
+          <Link to="/interview">Interview Tips</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Header
